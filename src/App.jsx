21de26c96710490bd3e0e62641d82410da705f62
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-09-11 21:52:00 +0600

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import Header from "./components/Header/Header";
 function App() {
   return (
     <>
+      <div className="container mx-auto py-6 px-2 lg:px-6">
       <Header></Header>
       <Blogs></Blogs>
+      </div>
     </>
   );
 }
